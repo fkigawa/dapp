4eefedce06054ffcb1bf0cce92369dcd98755953
Francis Kigawa <francis.kigawa@yale.edu>
2018-08-01 18:05:49 -0700

    login auth

--- src/Screens/AccountScreen.js
+++ src/Screens/AccountScreen.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Button, AsyncStorage} from 'react-native';
+import { LoginButton, AccessToken } from 'react-native-fbsdk';
+import homeNavigator from './MainTabs/HomeNavigator'
 
 export default class AccountScreen extends React.Component {
+
+  logout = async (key) => {
+     try {
+       await AsyncStorage.removeItem(key);
+       return true;
+     }
+     catch(exception) {
+       return false;
+     }
+    homeNavigator();
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Here's your Account boi</Text>
+        <Button onPress={() => this.logout()} title="logout"/>
       </View>
     );
   }
---
+++ src/Screens/MainTabs/HomeNavigator.js
@@ -0,0 +1,14 @@
+import { Navigation } from 'react-native-navigation';
+
+
+
+const homeNavigator = () => {
+  Navigation.startSingleScreenApp({
+      screen: {
+          screen: 'LoginScreen', // unique ID registered with Navigation.registerScreen
+          title: 'Welcome', // title of the screen as appears in the nav bar (optional)
+      },
+  });
+}
+
+export default homeNavigator
