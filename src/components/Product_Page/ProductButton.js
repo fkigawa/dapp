53ac807d7f41f6dd734d1a862662df90b91b0a9c
Salman Charolia <salmancharolia@gmail.com>
2018-08-02 14:59:51 -0700

    Adding add to cart functionality and server structure, bug in add to cart right now

---
+++ src/components/Product_Page/ProductButton.js
@@ -0,0 +1,74 @@
+import React from "react"
+import {Text, View, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import {addCart} from "../../store/actions/products";
+import {connect} from "react-redux"
+class ProductButton extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    onPressTile(data){
+        alert("item added");
+        this.props.addToCart(data);
+    }
+    render(){
+        return (
+        <View style={styles.container}>
+            {this.props.products.map((data,i)=>{
+                console.log(data);
+                return (
+                    <View>
+                    <Image source={data.image} style={styles.imageSize}/>
+                        <Text>{data.productName}</Text>
+                        <Text>{data.price}</Text>
+                    <TouchableOpacity key={i} onPress={(data)=>this.onPressTile(data)} style={styles.button} >
+                        <Text style={styles.addToCart}> Add To Cart </Text>
+                    </TouchableOpacity>
+                    </View>
+                )
+            })}
+        </View>
+    )}
+};
+
+const mapStateToProps = state =>{
+    return{
+        cartItems: state.root.cartItems
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        addToCart: (item) => dispatch(addCart(item))
+    }
+};
+
+export default connect(mapStateToProps,mapDispatchToProps)(ProductButton)
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'white',
+        justifyContent: 'space-between',
+        flexDirection: "row",
+        flexWrap: "wrap",
+        borderColor: "black",
+        borderWidth: 2,
+        margin: 10
+    },
+    button: {
+        backgroundColor: '#859a9b',
+        height: 20,
+        width:110,
+        alignItems: "center",
+    },
+    imageSize:{
+        height: 100,
+        width: 50,
+        alignItems: "center"
+    },
+    addToCart:{
+        fontSize: 11,
+        alignItems: "center"
+    }
+
+
+});
