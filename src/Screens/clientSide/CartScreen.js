d9888d2cef099a20d7dafd3deaf0ff40fd7f8425
Salman Charolia <salmancharolia@gmail.com>
2018-08-06 19:01:22 -0700

    Inserted checkout button and got it to connect to my backend

--- src/Screens/clientSide/CartScreen.js
+++ src/Screens/clientSide/CartScreen.js
@@ -1,22 +1,47 @@
 import React from 'react';
-import {StyleSheet, Text, View, ListView} from 'react-native';
+import {StyleSheet, Text, View, ListView, TouchableOpacity} from 'react-native';
 import {connect} from "react-redux"
 import {addCart} from "../../store/actions/products";
-
+let urlLink = "http://localhost:1337";
 
 class CartScreen extends React.Component {
+  checkoutButtonHandler(){
+      fetch(`${urlLink}/checkout`, {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json; charset=utf-8",
+          },
+          credentials: "include",
+          body: JSON.stringify({
+              cartItems: this.props.cartItems,
+              userId: this.props.userId
+          })
+      }).then((response) => {
+          return response.json();
+      })
+          .then((response) => {
+              if (response.success === true) {
+                  console.log("Response from backend at checkout is", response);
+              }
+          })
+  }
   render() {
     return (
+        <View style={styles.container}>
       <View style={styles.container}>
         <Text style={styles.welcome}>Here's your Cart!</Text>
           {this.props.cartItems.map((data,i)=> <Text style={styles.welcome} key={i}>{data.productName} {data.price}</Text>)}
       </View>
+            <TouchableOpacity style={styles.checkoutButton} onPress={()=>this.checkoutButtonHandler()}><Text>Checkout</Text></TouchableOpacity>
+        </View>
     );
   }
 }
 const mapStateToProps = state => {
+  console.log("USER ID IN MAP STATE TO PROPS IS", state.root.userId);
   return{
-    cartItems: state.root.cartItems
+    cartItems: state.root.cartItems,
+      userId: state.root.userId
   };
 };
 
@@ -39,4 +64,14 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
+    checkoutButton:{
+        backgroundColor: "#3b5998",
+        alignItems: 'center',
+        padding:5,
+        height: 30,
+        width: "90%",
+        margin: 10,
+        position: "absolute",
+        bottom: 0
+    }
 });
