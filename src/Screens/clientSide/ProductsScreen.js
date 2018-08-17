966244a3aab4fa2ca9c40896721dd2d0a9526891
Francis Kigawa <francis.kigawa@yale.edu>
2018-08-16 20:20:33 -0700

    back to category button

--- src/Screens/clientSide/ProductsScreen.js
+++ src/Screens/clientSide/ProductsScreen.js
@@ -9,13 +9,43 @@ import {initializingProducts,changingQuantity} from "../../store/actions/product
 import {Spinner} from "nachos-ui";
 import Icon from 'react-native-vector-icons/Feather';
 class ProductsScreen extends Component<Props> {
+  static navigatorButtons = {
+    leftButtons: [
+      {
+        title: '< Categories', // for a textual button, provide the button title (label)
+        id: 'back', // id for this button, given in onNavigatorEvent(event) to help understand which button was clicked
+        // testID: 'e2e_rules', // optional, used to locate this view in end-to-end tests
+        // disabled: true, // optional, used to disable the button (appears faded and doesn't interact)
+        // disableIconTint: true, // optional, by default the image colors are overridden and tinted to navBarButtonColor, set to true to keep the original image colors
+        // showAsAction: 'ifRoom', // optional, Android only. Control how the button is displayed in the Toolbar. Accepted valued: 'ifRoom' (default) - Show this item as a button in an Action Bar if the system decides there is room for it. 'always' - Always show this item as a button in an Action Bar. 'withText' - When this item is in the action bar, always show it with a text label even if it also has an icon specified. 'never' - Never show this item as a button in an Action Bar.
+        // buttonColor: 'blue', // Optional, iOS only. Set color for the button (can also be used in setButtons function to set different button style programatically)
+        // buttonFontSize: 14, // Set font size for the button (can also be used in setButtons function to set different button style programatically)
+        // buttonFontWeight: '600', // Set font weight for the button (can also be used in setButtons function to set different button style programatically)
+      },
+      {
+        // icon: Icon, // for icon button, provide the local image asset name
+        // id: 'add' // id for this button, given in onNavigatorEvent(event) to help understand which button was clicked
+      }
+    ]
+  };
+
   constructor(props) {
     super(props);
     this.state = ({
       products: [],
         done: false
     })
+    this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
   }
+
+  onNavigatorEvent(event) { // this is the onPress handler for the two buttons together
+    if (event.type == 'NavBarButtonPress') { // this is the event type for button presses
+      if (event.id == 'back') { // this is the same id field from the static navigatorButtons definition
+        categoryNavigator()
+      }
+    }
+  }
+
   componentDidMount = () => {
     fetch(`${urlLink}/products/${this.props.currentPage}`, {
         method: "GET",
