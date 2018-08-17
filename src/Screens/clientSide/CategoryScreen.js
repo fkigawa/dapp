eb35f1717852e4dcdb81067768b39954314028a0
Francis Kigawa <francis.kigawa@yale.edu>
2018-08-17 11:54:48 -0700

    get rid of location bar

--- src/Screens/clientSide/CategoryScreen.js
+++ src/Screens/clientSide/CategoryScreen.js
@@ -41,51 +41,6 @@ class CategoryScreen extends Component<Props> {
   }
 
     componentDidMount() {
-      Geocode.fromAddress("New York").then(
-        response => {
-          const { lat, lng } = response.results[0].geometry.location;
-          latInput = lat;
-          longInput = lng;
-        },
-        error => {
-          console.error('heree', error);
-        }
-      );
-
-      navigator.geolocation.requestAuthorization()
-
-      navigator.geolocation.getCurrentPosition(
-          (position) => {
-            Geocode.setApiKey(geoKey);
-
-            Geocode.fromLatLng(position.coords.latitude, position.coords.longitude).then(
-              response => {
-                const address = response.results[0].formatted_address;
-                this.setState({
-                  myAddress: address,
-                    done: true
-                });
-                console.log(this.state.myAddress)
-              },
-              error => {
-                console.error(error);
-              }
-            );
-
-            let distance = geolib.getDistance(position.coords, {
-                latitude: latInput,
-                longitude: longInput
-            });
-            distance *= 0.000621371
-            if (distance > 2) {
-              return tooFar = true
-              // return alert('Not within delivery radius')
-            }
-          },
-          function() {
-              alert('Position could not be determined.')
-          }, options
-      );
     }
 
     changeAddress = (myAddress) => {
@@ -98,15 +53,6 @@ class CategoryScreen extends Component<Props> {
     render() {
         return (
           <View>
-            {(tooFar) ?
-            <View style={styles.address}>
-              <TextInput clearButtonMode="while-editing" placeholder="Enter your address" value='Sorry, we do not deliver to your location yet!' onChangeText={(event)=>this.changeAddress(event)}/>
-            </View>
-            :
-            <View style={styles.address}>
-              <TextInput clearButtonMode="while-editing" placeholder="Enter your address" value={this.state.myAddress} onChangeText={(event)=>this.changeAddress(event)}/>
-            </View>
-            }
             <View style={styles.container}>
               <ProductCategoryButton changeCategory={this.changeCategoryHandler} key={Math.random()}/>
             </View>
